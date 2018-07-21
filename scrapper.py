579b88759acd999e145aae0f67a1a1cc468563a5
James Wellnitz <37743839+jimmyjbling@users.noreply.github.com>
2018-07-20 19:56:00 -0500

    Added navigation to programs page

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.support import ui
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import requests
 
@@ -20,6 +22,7 @@ def login(user_psw):
     psw_field = driver.find_element_by_id('UserPassword')
     psw_field.send_keys(user_psw[1])
     psw_field.send_keys(Keys.RETURN)
+    get_program_list(driver)
 
 
 def page_is_loaded(driver):
@@ -34,6 +37,24 @@ def login_info():
     user = input('Username: ')
     psw = input('Password: ')
     return [user, psw]
+
+
+def make_soup(url):
+    """
+    parses web page into beautiful soup
+    :param: url of web page
+    :return: soup of web page html
+    """
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    return soup
+
+
+def get_program_list(driver):
+    wait = ui.WebDriverWait(driver, 10)
+    wait.until(expected_conditions.presence_of_element_located((By.ID, 'MasterHeaderCtl_liPrograms')))
+    link = driver.find_element_by_id('MasterHeaderCtl_liPrograms')
+    link.click()
 
 
 login(login_info())
